f8a33cde4c7f25843ef82cb40d7d96883b1a7b11
frankpagan <frank@cocreate.app>
2024-01-05 04:03:44 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
-const Https = require('https');
 const Http = require('http');
-const fs = require('fs');
+const Https = require('https');
 const tls = require('tls');
 const acme = require('@cocreate/acme')
+const fs = require('fs');
+
+// const organizations = {}
+// const hosts = {}
 
 let server = {
-    acme: new acme(),  // Initially an empty object or some placeholder functionality
-
-    loadCertificates(domain) {
-        try {
-            return {
-                key: fs.readFileSync(`/etc/certificates/${domain}/private-key.pem`),
-                cert: fs.readFileSync(`/etc/certificates/${domain}/fullchain.pem`),
-            };
-        } catch (error) {
-            console.error("Error loading certificates for domain:", domain);
-            throw error;  // Or handle it by returning default certificates
-        }
-    },
-
-    async sniCallback(domain, cb) {
-        try {
-            console.log('sni')
-            await server.acme.checkCertificate(domain);  // Referencing `this.acme` 
-
-            const sslContext = tls.createSecureContext(server.loadCertificates(domain));
-            cb(null, sslContext);
-        } catch (error) {
-            console.error("Error in SNI callback for domain:", domain, error);
-            cb(error);  // handle error or use default context
-        }
-    },
-
-    https: null,  // Will be set after defining `sniCallback`
-    http: Http.createServer()
+    acme: new acme(),
+    http: Http.createServer(),
+    https: Https.createServer({ SNICallback: sniCallback })
 };
 
-// Creating the HTTPS server with the SNI callback
-server.https = Https.createServer({ SNICallback: server.sniCallback });
+function loadCertificates(domain) {
+    try {
+        return {
+            key: fs.readFileSync(`/etc/certificates/${domain}/private-key.pem`),
+            cert: fs.readFileSync(`/etc/certificates/${domain}/fullchain.pem`),
+        };
+    } catch (error) {
+        console.error("Error loading certificates for domain:", domain);
+        throw error;  // Or handle it by returning default certificates
+    }
+}
+
+async function sniCallback(domain, cb) {
+    try {
+        console.log('sni')
+        await server.acme.checkCertificate(domain);  // Referencing `this.acme` 
+
+        const sslContext = tls.createSecureContext(loadCertificates(domain));
+        cb(null, sslContext);
+    } catch (error) {
+        console.error("Error in SNI callback for domain:", domain, error);
+        cb(error);  // handle error or use default context
+    }
+}
 
 module.exports = server;
